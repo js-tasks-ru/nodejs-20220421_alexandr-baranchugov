dc7ae5b9fed618c9daa11a9476bc2ecd160a7050
baranchugov.alexandr <baranchugov.alexandr@otr.ru>
2022-05-22 18:56:23 +0300

    task 3.3

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -12,6 +13,16 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Internal server error');
+      } else if (fs.existsSync(filepath)) {
+        res.statusCode = 200;
+        res.end(fs.readFileSync(filepath));
+      } else {
+        res.statusCode = 404;
+        res.end('Not found');
+      }
 
       break;
 
