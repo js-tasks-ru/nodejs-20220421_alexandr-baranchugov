0f0a38c0343368f7f0a93da4d552444f71b7c554
baranchugov.alexandr <baranchugov.alexandr@otr.ru>
2022-06-05 05:29:30 +0300

    Task 7.1

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,21 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
-    {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
-    },
+  {usernameField: 'email', session: false},
+  async function(email, password, done) {
+    const user = await User.findOne({email});
+
+    if (!user) {
+      done(null, false, 'Нет такого пользователя');
+      return;
+    }
+
+    if (await user.checkPassword(password)) {
+      console.log('Valid password');
+      done(null, user);
+    } else {
+      done(null, false, 'Неверный пароль');
+    }
+  },
 );
