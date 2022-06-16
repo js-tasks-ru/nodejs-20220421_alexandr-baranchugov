5f682a34a937a2dd85e7e3f73a2e2419f14ad975
baranchugov.alexandr <baranchugov.alexandr@otr.ru>
2022-06-16 14:53:44 +0300

    Task 9.1

--- 09-websockets/01-chat/controllers/messages.js
+++ 09-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,26 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {};
+  if (!ctx.user) {
+    ctx.status = 401;
+    ctx.body = {error: 'Пользователь не залогинен'};
+    return;
+  }
+
+  console.log(ctx.user.id)
+
+  const messages = await Message.find({chat: ctx.user.id});
+
+  ctx.body = {
+    messages: messages.map(messageMap),
+  };
 };
+
+function messageMap({_id, date, text, user}) {
+  return {
+    date,
+    text,
+    user,
+    id: _id.toString(),
+  };
+}
